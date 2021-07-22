5604bf9f86378ff1fb72422f4b1d1941c4e278b8
Felix44 <felice.dangelo2013@gmail.com>
2021-07-22 23:17:07 +0200

    added economy Object test

---
+++ economy.js
@@ -0,0 +1,54 @@
+class EconomySystem {
+    constructor(username) {
+        this.money = 0
+        this.rank = 0
+        this.user = username
+    }
+
+    check() {
+
+    }
+
+    give(amount, message) {
+        this.money = this.money + amount
+        if (message) {
+            message.channel.send(this.user + " gained " + amount + " DogeCoins!")
+        }
+    }
+
+    take(amount) {
+        this.money = Math.max(this.money - amount, 0)
+    }
+
+    rankup(message) {
+        let needed = 100 + (100 * this.rank)
+        if (this.money >= needed) {
+            this.rank = this.rank + 1
+            this.take(needed)
+            if (message) {
+                message.channel.send(this.user + " is now rank " + this.rank + "")
+            }
+        }
+    }
+}
+
+Economy = {
+    list: {},
+    check: (user) => {
+        if (!Economy.list[user.id]) {
+            Economy.list[user.id] = new EconomySystem(user.username)
+        }
+    },
+    give: (amount, message) => {
+        let user = message.author
+        Economy.check(user)
+        Economy.list[user.id].give(amount, message)
+    },
+    take: (amount, message) => {
+        let user = message.author
+        Economy.check(user)
+        if (Economy.list[user.id].check()) {
+            Economy.list[user.id].take(amount, message)
+        }
+    } 
+}
