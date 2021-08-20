4a7b68d92406a267915b74f687b7c26ed240c3cf
Felix44 <felice.dangelo2013@gmail.com>
2021-08-20 11:53:04 +0200

    even more console stuff
    when will I stop

--- src/prefix.js
+++ src/prefix.js
@@ -19,24 +19,39 @@ Prefix = {
   }
 }
 
-Commands.prefix = new Command('Changes the prefix for the current server. Put `default` as the argument of `prefix` to reset the current server-prefix to the global prefix\nThe user needs \"Manage Guild\" permissions for this command', (message , args) => {
-    if (debugmode) {
-      console.log("- " + Colors.blue.colorize("Update of \"prefixes.json\" was cancelled due to debug mode being active"))
-      throw ("This command cannot be used while in debug mode")
-    }
-    if (!message.member.permissions.has("MANAGE_GUILD")) {
-      console.log("- " + Colors.blue.colorize("Update of \"prefixes.json\" was cancelled due to the user not having \"Manage Guild\" permissions"))
-      throw ("You need \"Manage Guild\" permissions for this command")
-    }
-    fs.writeFileSync(
-        "./src/Data/prefixes.json",
-        JSON.stringify(
-            {
-                ...Prefix.read(),
-                [message.guild.id]: args[0] == 'default' ? undefined: args[0]
-            }, null, 4
-        ),
-        "utf8"
-    )
-    console.log("- " + Colors.purple.colorize("Successfully updated file \"prefixes.json\""))
+Commands.prefix = new Command('Changes the prefix for the current server. Put `default` as the argument of `prefix` to reset the current server-prefix to the global prefix\nThe user needs \"Manage Guild\" permissions for this command', (message, args) => {
+  if (debugmode) {
+    console.log("- " + Colors.blue.colorize("Aborted prefix change due to debug mode being active:") +
+      "\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
+      "\n\tRequested prefix: " + args[0] +
+      "\n\tGuild name: " + message.guild.name +
+      "\n\tGuild ID: " + message.guild.id)
+    console.log("- " + Colors.blue.colorize("Update of \"prefixes.json\" was cancelled due to debug mode being active"))
+    throw ("This command cannot be used while in debug mode")
+  }
+  if (!message.member.permissions.has("MANAGE_GUILD")) {
+    console.log("- " + Colors.blue.colorize("Aborted prefix change due to the requesting user not having \"Manage Guild\" permissions") +
+      "\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
+      "\n\tRequested prefix: " + args[0] +
+      "\n\tGuild name: " + message.guild.name +
+      "\n\tGuild ID: " + message.guild.id)
+    console.log("- " + Colors.blue.colorize("Update of \"prefixes.json\" was cancelled due to the user not having \"Manage Guild\" permissions"))
+    throw ("You need \"Manage Guild\" permissions for this command")
+  }
+  console.log("- " + Colors.cyan.colorize("Successfully updated server prefix:") +
+    "\n\tOld prefix: " + Prefix.get(message.guild.id) +
+    "\n\tNew prefix: " + args[0] +
+    "\n\tGuild name: " + message.guild.name +
+    "\n\tGuild ID: " + message.guild.id)
+  fs.writeFileSync(
+    "./src/Data/prefixes.json",
+    JSON.stringify(
+      {
+        ...Prefix.read(),
+        [message.guild.id]: args[0] == 'default' ? undefined : args[0]
+      }, null, 4
+    ),
+    "utf8"
+  )
+  console.log("- " + Colors.purple.colorize("Successfully updated file \"prefixes.json\""))
 }, 'Utility', [new RequiredArg(0, 'Missing `prefix` argument', 'prefix')])
