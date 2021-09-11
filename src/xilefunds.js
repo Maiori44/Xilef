696e0a7fb45529860fc0f02065f498452f66e1db
Felix44 <felice.dangelo2013@gmail.com>
2021-09-11 20:05:42 +0200

    &stocks fix 2

--- src/xilefunds.js
+++ src/xilefunds.js
@@ -44,7 +44,7 @@ Commands.stocks = new Command("Buy and sell Xilefunds\n\n" + Stocks.help, (messa
                 .setTimestamp()
             if (Stocks.auction.seller) {
                 StocksEmbed.addField("Current auction:", `Seller: ${Stocks.auction.seller}\nLatest offer: ${Stocks.auction.buyer ? (Stocks.auction.buyer + " -> " + Stocks.auction.price) : "`none`"}`)            
-                StocksEmbed.setFooter("The auction will end in " + Time.converTime(Math.floor(Stocks.timeout._idleTimeout - ((process.uptime() * 1000) - Stocks.timeout._idleStart))))
+                StocksEmbed.setFooter("The auction will end in " + Time.convertTime(Math.floor(Stocks.timeout._idleTimeout - ((process.uptime() * 1000) - Stocks.timeout._idleStart))))
             } else StocksEmbed.setFooter("Start an auction to sell your Xilefund!")
             message.channel.send(StocksEmbed)
             return
