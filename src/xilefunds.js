266feeec2da39b12d6fb536a25834fc41d8093e2
Felix44 <felice.dangelo2013@gmail.com>
2021-09-13 15:07:48 +0200

    small fix for the text that says the lowest possible offer

--- src/xilefunds.js
+++ src/xilefunds.js
@@ -43,7 +43,7 @@ Commands.stocks = new Command("Buy and sell Xilefunds\n\n" + Stocks.help, (messa
                 .setDescription(msg)
                 .setTimestamp()
             if (Stocks.auction.seller) {
-                StocksEmbed.addField("Current auction:", `Seller: ${Stocks.auction.seller}\nLatest offer: ${Stocks.auction.buyer ? (Stocks.auction.buyer + " -> " + Stocks.auction.price) : "`none` (lowest offer possible: " + Stocks.auction.price + ")"}`)            
+                StocksEmbed.addField("Current auction:", `Seller: ${Stocks.auction.seller}\nLatest offer: ${Stocks.auction.buyer ? (Stocks.auction.buyer + " -> " + Stocks.auction.price) : "`none` (lowest offer possible: " + (Stocks.auction.price + 1) + ")"}`)            
                 StocksEmbed.setFooter("The auction will end in " + Time.convertTime(Math.floor(Stocks.timeout._idleTimeout - ((process.uptime() * 1000) - Stocks.timeout._idleStart))))
             } else StocksEmbed.setFooter("Start an auction to sell your Xilefund!")
             message.channel.send(StocksEmbed)
