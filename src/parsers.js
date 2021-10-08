9922e5b439de4fb01bf225f2f14bf3a28f0e9acf
Felix44 <felice.dangelo2013@gmail.com>
2021-10-08 21:46:47 +0200

    not realted to aliases but fixed prefixes console messages

--- src/parsers.js
+++ src/parsers.js
@@ -22,20 +22,18 @@ Prefix = {
 
 Commands.prefix = new Command('Changes the prefix for the current server. Put `default` as the argument of `prefix` to reset the current server-prefix to the global prefix\nThe user needs \"Manage Guild\" permissions for this command', (message, args) => {
 	if (debugmode) {
-		console.log("- " + Colors.blue.colorize("Aborted server prefix update:") +
+		console.log("- " + Colors.blue.colorize("Aborted server prefix update due to Debug mode:") +
 			"\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
 			"\n\tRequested prefix: " + args[0] +
-			"\n\tReason: Debug mode is active" +
 			"\n\tGuild name: " + message.guild.name +
 			"\n\tGuild ID: " + message.guild.id)
 		console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to debug mode being active"))
 		throw ("This command cannot be used while in debug mode")
 	}
 	if (!message.member.permissions.has("MANAGE_GUILD")) {
-		console.log("- " + Colors.blue.colorize("Aborted server prefix update:") +
+		console.log("- " + Colors.blue.colorize("Aborted server prefix update due to missing permissions:") +
 			"\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
 			"\n\tRequested prefix: " + args[0] +
-			"\n\tReason: The user does not have \"Manage Guild\" permissions" +
 			"\n\tGuild name: " + message.guild.name +
 			"\n\tGuild ID: " + message.guild.id)
 		console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to the user not having \"Manage Guild\" permissions"))
