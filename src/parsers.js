1ac1d7c63292a007b02a37689e9d60df863c0542
LJMSU <67260083+LJMSU@users.noreply.github.com>
2021-10-18 19:41:42 +0700

    fix(alias): fix delete menu

--- src/parsers.js
+++ src/parsers.js
@@ -1,62 +1,62 @@
 const { RequiredArg, Command } = require("./commands.js")
 const fs = require('fs')
 const { Console } = require("console")
-const { MessageMenu } = require("discord-buttons")
+const { MessageMenu, MessageComponent } = require("discord-buttons")
 
 Prefix = {
-	/**
-	 * @param {string} guildID
-	 * @returns {string} - The guild's prefix. fallbacks to the global prefix (client.prefix)
-	 */
-	get(guildID) {
-		return debugmode ? "beta&" : (this.read()[guildID] ?? client.prefix)
-	},
+  /**
+   * @param {string} guildID
+   * @returns {string} - The guild's prefix. fallbacks to the global prefix (client.prefix)
+   */
+  get(guildID) {
+    return debugmode ? "beta&" : (this.read()[guildID] ?? client.prefix)
+  },
 
-	/**
-	 * Gets the whole prefixes.json
-	 * @returns {{[string in string]: string}}
-	 */
-	read() {
-		return JSON.parse(fs.readFileSync("./src/Data/prefixes.json", "utf8"))
-	}
+  /**
+   * Gets the whole prefixes.json
+   * @returns {{[string in string]: string}}
+   */
+  read() {
+    return JSON.parse(fs.readFileSync("./src/Data/prefixes.json", "utf8"))
+  }
 }
 
 Commands.prefix = new Command('Changes the prefix for the current server. Put `default` as the argument of `prefix` to reset the current server-prefix to the global prefix\nThe user needs \"Manage Guild\" permissions for this command', (message, args) => {
-	if (debugmode) {
-		console.log("- " + Colors.blue.colorize("Aborted server prefix update due to Debug mode:") +
-			"\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
-			"\n\tRequested prefix: " + args[0] +
-			"\n\tGuild name: " + message.guild.name +
-			"\n\tGuild ID: " + message.guild.id)
-		console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to debug mode being active"))
-		throw ("This command cannot be used while in debug mode")
-	}
-	if (!message.member.permissions.has("MANAGE_GUILD")) {
-		console.log("- " + Colors.blue.colorize("Aborted server prefix update due to missing permissions:") +
-			"\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
-			"\n\tRequested prefix: " + args[0] +
-			"\n\tGuild name: " + message.guild.name +
-			"\n\tGuild ID: " + message.guild.id)
-		console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to the user not having \"Manage Guild\" permissions"))
-		throw ("You need \"Manage Guild\" permissions for this command")
-	}
-	console.log("- " + Colors.cyan.colorize("Successfully updated server prefix:") +
-		"\n\tOld prefix: " + Prefix.get(message.guild.id) +
-		"\n\tNew prefix: " + args[0] +
-		"\n\tGuild name: " + message.guild.name +
-		"\n\tGuild ID: " + message.guild.id)
-	fs.writeFileSync(
-		"./src/Data/prefixes.json",
-		JSON.stringify(
-			{
-				...Prefix.read(),
-				[message.guild.id]: args[0] == 'default' ? undefined : args[0]
-			}, null, 4
-		),
-		"utf8"
-	)
-	message.channel.send("This server's prefix is now `" + Prefix.get(message.guild.id) + "`")
-	console.log("- " + Colors.purple.colorize("Successfully updated file ") + Colors.hpurple.colorize("prefixes.json"))
+  if (debugmode) {
+    console.log("- " + Colors.blue.colorize("Aborted server prefix update due to Debug mode:") +
+      "\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
+      "\n\tRequested prefix: " + args[0] +
+      "\n\tGuild name: " + message.guild.name +
+      "\n\tGuild ID: " + message.guild.id)
+    console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to debug mode being active"))
+    throw ("This command cannot be used while in debug mode")
+  }
+  if (!message.member.permissions.has("MANAGE_GUILD")) {
+    console.log("- " + Colors.blue.colorize("Aborted server prefix update due to missing permissions:") +
+      "\n\tCurrent prefix: " + Prefix.get(message.guild.id) +
+      "\n\tRequested prefix: " + args[0] +
+      "\n\tGuild name: " + message.guild.name +
+      "\n\tGuild ID: " + message.guild.id)
+    console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("prefixes.json") + Colors.blue.colorize(" was cancelled due to the user not having \"Manage Guild\" permissions"))
+    throw ("You need \"Manage Guild\" permissions for this command")
+  }
+  console.log("- " + Colors.cyan.colorize("Successfully updated server prefix:") +
+    "\n\tOld prefix: " + Prefix.get(message.guild.id) +
+    "\n\tNew prefix: " + args[0] +
+    "\n\tGuild name: " + message.guild.name +
+    "\n\tGuild ID: " + message.guild.id)
+  fs.writeFileSync(
+    "./src/Data/prefixes.json",
+    JSON.stringify(
+      {
+        ...Prefix.read(),
+        [message.guild.id]: args[0] == 'default' ? undefined : args[0]
+      }, null, 4
+    ),
+    "utf8"
+  )
+  message.channel.send("This server's prefix is now `" + Prefix.get(message.guild.id) + "`")
+  console.log("- " + Colors.purple.colorize("Successfully updated file ") + Colors.hpurple.colorize("prefixes.json"))
 }, 'Utility', [new RequiredArg(0, 'Missing `prefix` argument', 'prefix')])
 
 aliasesMenus = {}
@@ -74,11 +74,11 @@ aliases = new class extends Discord.Collection {
         });
     }
     save() {
-		if (debugmode) {
-			console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("aliases.json") + Colors.blue.colorize(" was cancelled due to debug mode being active"))
-			return
-		}
+        if (debugmode) {
+            console.log("- " + Colors.blue.colorize("Update of ") + Colors.hblue.colorize("aliases.json") + Colors.blue.colorize(" was cancelled due to debug mode being active"))
+        }
         const json = JSON.stringify(Object.fromEntries(this), null, 4);
+        console.log(json);
         fs.writeFileSync("./src/Data/aliases.json", json, "utf8");
     }
     updateMenu(id) {
@@ -95,11 +95,12 @@ aliases = new class extends Discord.Collection {
         }
         for (const name in Aliases) {
             const substitute = Aliases[name]
-            AliasList.addOption({label: name, description: substitute, value: name})
+            AliasList.addOption({label: name, description: 'alias', value: name})
         }
         message.edit("** **", AliasList)
     }
 }
+
 const aliasHelp = `
 \`&alias set (name) (substitute)\` set an alias for \`substitute\` with the name \`name\`
 \`&alias get (name)\` get an alias with the name \`name\`
@@ -127,10 +128,10 @@ Commands.alias = new Command("Manage command aliases\n\n" + aliasHelp, (message,
                 [name]: String(substitute)
             }).save();
 
-			console.log("- " + Colors.cyan.colorize("Successfully added alias:") +
-				"\n\tAlias name: " + name +
-				"\n\tAlias substitute: " + substitute +
-				"\n\tUser: " + message.author.username)
+      console.log("- " + Colors.cyan.colorize("Successfully added alias:") +
+        "\n\tAlias name: " + name +
+        "\n\tAlias substitute: " + substitute +
+        "\n\tUser: " + message.author.username)
 
             message.channel.send("Alias set successfully");
             break;
@@ -189,12 +190,12 @@ Commands.alias = new Command("Manage command aliases\n\n" + aliasHelp, (message,
 }, "Utility", [
     new RequiredArg(0, aliasHelp, "command"),
     new RequiredArg(1, undefined, "argument 1", true),
-	new RequiredArg(1, undefined, "argument 2", true),
+  new RequiredArg(1, undefined, "argument 2", true),
 ])
-
-client.on('clickMenu', async (menu) => {
+client.on('clickMenu', async (/** @type {MessageComponent} */ menu) => {
     if (menu.values[0] == "empty") return
-    delete aliases.get(menu.id)[menu.values[0]]
+    delete aliases.get(menu.clicker.id)[menu.values[0]]
+    aliases.save();
     aliases.updateMenu(menu.id)
     await menu.reply.defer()
 })
