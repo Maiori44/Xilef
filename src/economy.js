e33e2116c662d9aa895f3f123e7928d67cc12028
Felix44 <felice.dangelo2013@gmail.com>
2021-12-08 23:29:05 +0100

    ok ACTUALLY fixed taxes

--- src/economy.js
+++ src/economy.js
@@ -126,7 +126,7 @@ class EconomySystem {
         if (this.money >= amount) {
             const tax = Math.round(amount * (this.rank < 500 ? 0 : this.rank - 500) / 100);
 
-            this.money = Math.min(this.money - (notax ? 0 : tax), this.money);
+            this.money = Math.max(money - amount - (notax ? 0 : tax), 0)
             if (message && tmsg) {
                 message.channel.send(tmsg)
                 message.channel.send(`The purchase costed ${amount} (tax: ${(this.rank < 500 ? 0 : this.rank - 500)}%)`)
