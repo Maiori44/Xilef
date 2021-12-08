d68bcf71347e7f696cd005831cab971687c3c9ee
Felix44 <felice.dangelo2013@gmail.com>
2021-12-08 23:30:38 +0100

    and fixed even more cause why not

--- src/economy.js
+++ src/economy.js
@@ -124,12 +124,11 @@ class EconomySystem {
             return false
         }
         if (this.money >= amount) {
-            const tax = Math.round(amount * (this.rank < 500 ? 0 : this.rank - 500) / 100);
-
-            this.money = Math.max(money - amount - (notax ? 0 : tax), 0)
+            const tax = notax ? 0 : Math.round(amount * (this.rank < 500 ? 0 : this.rank - 500) / 100)
+            this.money = Math.max(money - amount - tax, 0)
             if (message && tmsg) {
                 message.channel.send(tmsg)
-                message.channel.send(`The purchase costed ${amount} (tax: ${(this.rank < 500 ? 0 : this.rank - 500)}%)`)
+                message.channel.send(`The purchase costed ${amount + tax} (tax: ${(this.rank < 500 ? 0 : this.rank - 500)}%)`)
             }
 
             console.log("- " + Colors.purple.colorize("Sucessful attempt at buying from an EconomySystem:") +
