97fe7b51c3d33c88b9806f47b4dc0cf34f9238e9
Felix44 <felice.dangelo2013@gmail.com>
2021-12-08 23:21:00 +0100

    finally fixed taxes

--- src/economy.js
+++ src/economy.js
@@ -124,9 +124,9 @@ class EconomySystem {
             return false
         }
         if (this.money >= amount) {
-            const tax = amount * Math.round((this.rank < 500 ? 0 : this.rank - 500) / 100);
+            const tax = Math.round(amount * (this.rank < 500 ? 0 : this.rank - 500) / 100);
 
-            this.money = this.money - Math.min(this.money - (notax ? 0 : tax), this.money);
+            this.money = Math.min(this.money - (notax ? 0 : tax), this.money);
             if (message && tmsg) {
                 message.channel.send(tmsg)
                 message.channel.send(`The purchase costed ${amount} (tax: ${(this.rank < 500 ? 0 : this.rank - 500)}%)`)
