90264e76847cd265ca2517e2429aca397c0297d7
unholy-codeism <re.nemesic@outlook.com>
2021-12-17 14:46:58 +0100

    cleanup: delete src/commands/test.js

--- src/commands/test.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('../command.js')
-
-module.exports = new Command({
-    name: "test",
-    description: "test",
-    async run(message, args, client) {
-        message.channel.send("test")
-    }
-})
