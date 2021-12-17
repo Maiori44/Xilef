fa49e980d7930f467aef106d4b0421c9f9309082
unholy-codeism <re.nemesic@outlook.com>
2021-12-17 15:37:32 +0100

    feat: some more logging for ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,7 @@
 const { Event } = require('../event.js')
+const { ColorMap } = require('../logger.js')
 
 module.exports = new Event("ready", client => {
-    console.log('ready!')
+    client.logger._write("Client is now ready\n\n", "clientReady")
+    if (client.debugging) client.logger.warning("The bot is in debug mode, changes are not to be saved.")
 })
