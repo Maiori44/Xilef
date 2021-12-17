db1ac0a858e91789c9ea1b9bd38909dbf47aaef1
unholy-codeism <re.nemesic@outlook.com>
2021-12-17 21:17:30 +0100

    rewrite: create images.js + jokes.js

---
+++ src/commands/images.js
@@ -0,0 +1,99 @@
+const { Command } = require('../command.js')
+const { MessageAttachment } = require('discord.js')
+
+const chadImage = new MessageAttachment("./src/commands/images/giga chad.jpg")
+const amogusimage = new MessageAttachment("./src/Pictures/amogus.gif")
+const sdrogoimage = new Discord.MessageAttachment("./src/Pictures/sdrogo.jpg")
+const vshitimage = new Discord.MessageAttachment("./src/Pictures/vshit.png")
+const uwuimage = new Discord.MessageAttachment("./src/Pictures/uwu.png")
+const horseimages = [
+    new Discord.MessageAttachment("./src/Pictures/Horse/horse1.jpg"),
+    new Discord.MessageAttachment("./src/Pictures/Horse/horse2.jpg"),
+    new Discord.MessageAttachment("./src/Pictures/Horse/horse3.jpg"),
+    new Discord.MessageAttachment("./src/Pictures/Horse/horse4.jpg"),
+    new Discord.MessageAttachment("./src/Pictures/Horse/horse5.jpg"),
+]
+
+module.exports = [
+    new Command({
+        name: "chad",
+        description: "Sends a beautiful giga chad",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send({
+                content: "epik",
+                files: [chadImage]
+            })
+        }
+    }),
+    new Command({
+        name: "amogus",
+        description: "It's pretty sus",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send({
+                content: "sus",
+                files: [amogusimage]
+            })
+        }
+    }),
+    new Command({
+        name: "sdrogo",
+        description: "Sdrogo man is da wae",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send({
+                content: "hm yes",
+                files: [sdrogoimage]
+            })
+        }
+    }),
+    new Command({
+        name: "vshit",
+        description: "Vsauce here",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send({
+                content: "hey vsauce, Michael here, could you get out of my bathroom?",
+                files: [vshitimage]
+            })
+        }
+    }),
+    new Command({
+        name: "uwu",
+        description: "Sends a super cute kawaii image ^w^",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send({
+                content: ":3",
+                files: [uwuimage]
+            })
+        }
+    }),
+    new Command({
+        name: "horse",
+        description: "Sends a random horse photo, idk why i made this",
+        category: "Images",
+        async run(message, args) {
+            let selection = Math.floor(Math.random() * 6)
+            message.channel.send({
+                content: "have a horse i guess",
+                files: [horseimages[selection]]
+            })
+        }
+    }),
+    new Command({
+        name: "whoasked",
+        description: "Finds the person who asked with the brand new OmegaSearch algorithm",
+        category: "Images",
+        async run(message, args) {
+            message.channel.send("https://tenor.com/view/meme-who-asked-satellite-looking-radar-gif-17171784")
+            message.channel.send("Asking neighbours if they know who asked...")
+            setTimeout(() => { message.channel.send("Asking the government if they know who asked...") }, 3000)
+            setTimeout(() => { message.channel.send("Asking the aliens if they know who asked...") }, 6000)
+            setTimeout(() => { message.channel.send("Asking god if they know who asked...") }, 9000)
+            setTimeout(() => { message.channel.send("Asking google if they know who asked...") }, 12000)
+            setTimeout(() => { message.channel.send("Yeah no, nobody asked.") }, 15000)
+        }
+    }),
+]
---
+++ src/commands/jokes.js
@@ -0,0 +1,74 @@
+const { Command } = require('../command.js')
+
+module.exports = [
+    new Command({
+        name: "hi",
+        description: "Says hi to you",
+        category: "Joke",
+        async run(message, args, client) {
+            message.reply("Hi.")
+        }
+    }),
+    new Command({
+        name: "annoy",
+        description: "Annoys the person you want",
+        category: "Joke",
+        async run(message, args, client) {
+            message.channel.send(args[0].slice(0, args[0].length) + " you suck")
+        }
+    }),
+    new Command({
+        name: "comfort",
+        description: "Comforts the person you want",
+        category: "Joke",
+        async run(message, args, client) {
+            message.channel.send(args[0].slice(0, args[0].length) + " you don't suck")
+        }
+    }),
+    new Command({
+        name: "say",
+        description: "Says whatever you want",
+        category: "Joke",
+        async run(message, args, client) {
+            if (!args.join(" ")) args[0] = "** **"
+            message.channel.send(args.join(" ").slice(0, 1900))
+            message.delete()
+        }
+    }),
+    new Command({
+        name: "hentai",
+        description: "Totally sends you hentai",
+        category: "Joke",
+        async run(message, args, client) {
+            message.channel.send("No... just no...")
+        }
+    }),
+    new Command({
+        name: "secret",
+        description: "Sends you the secret of life",
+        category: "Joke",
+        async run(message, args, client) {
+            message.author.send("The secret of life is...\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀||\n" +
+                "||⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀||\n" +
+                "||⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀||\n" +
+                "||⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀||\n" +
+                "||⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀||\n" +
+                "||⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀||\n" +
+                "||⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀||\n" +
+                "||⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀||\n" +
+                "||⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀||\n" +
+                "||⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀||\n" +
+                "||⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀||\n" +
+                "||⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀||\n" +
+                "|| amogus. ||")
+        }
+    })
+]
