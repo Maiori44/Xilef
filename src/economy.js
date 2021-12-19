c4eaa3e54ed67ef89cc75569c505ea64d99dafc1
unholy-codeism <re.nemesic@outlook.com>
2021-12-19 14:58:11 +0100

    feat: create equivalent of FlagSystem; cleanup: use a more efficient method to check for fields in XilefUser; (hot?)fix: work around BSON incorrectly deserializing the new UserBinaryFlags; fix: do not export Balance anymore

--- src/economy.js
+++ src/economy.js
@@ -4,26 +4,79 @@ const fs = require('fs')
 const BSON = require('bson')
 const saveFilePath = './src/data/economy.bson'
 
+class UserBinaryData {
+    /**
+     * 
+     * @param {String} binary The binary read from the JSON
+     */
+    constructor(binary) {
+        /**
+         * @type {Number}
+         */
+        this.binary = binary ? Number(binary) : Number(0)
+    }
+
+    getBinary() {
+        return this.binary.toString(2)
+    }
+
+    /**
+     * Will set the bit at `position` (set to 1)
+     * @param {Number} position 
+     */
+    setflagAt(position) { 
+        this.binary |= 1 << position // cool operator i found
+    }
+
+    /**
+     * Will clear the bit at `position` (set to 0)
+     * @param {Number} position 
+     */
+    clearFlagAt(position) {
+        this.binary &= ~(1 << position)
+    }
+
+    /**
+     * 
+     * @param {Number} position 
+     * @returns {boolean}
+     */
+    isFlagSet(position) {
+        return ((this.binary >> position) & 1) == 1
+    }
+
+    getFormattedBinary(replace1, replace0) {
+        replace1 = replace1 || []
+        replace1.reverse()
+        let bits = [...this.binary.toString(2).padStart(this.totalflags, "0")]
+        bits.forEach((bit, position) => {
+            if (bit == "1") {
+                bits[position] = replace1[position] || "1"
+            } else {
+                bits[position] = replace0 || "0"
+            }
+        })
+        replace1.reverse()
+        return bits.join("")
+    }
+}
+
 class XilefUser {
     constructor(options) {
-        // check if options has these keys (below)
-        const requiredKeys = ["money", "rank"]
-        requiredKeys.booleanCorrespondent = requiredKeys.map(() => false)
-
-        Object.keys(options).forEach((key, index) => {
-            if (options[key] && requiredKeys.includes(key))
-                requiredKeys.booleanCorrespondent[index] = true
-        })
-
-        if (requiredKeys.booleanCorrespondent.find(b => b == false))
-            throw new Error("Required key of XilefUser not found.")
-        // if every element of requireKeys matches with a key of the options param, then it won't throw an error
-        // this allows to expand the list of required keys without too much work in case the bot grows bigger whilst also keeping the code somewhat clean (better than `if (!options.money)`... for each key we wanna check)
+        const optionsKeys = Object.keys(options)
+        if (
+            !optionsKeys.includes("money") ||
+            !optionsKeys.includes("rank")
+        )
+            throw new Error("Missing required key of XilefUser")
 
         this.money = options.money
         this.rank = options.rank
+
+        this.vgot = new UserBinaryData(options.vgot.binary || "0")
     }
 }
+
 /**
  * @typedef {{logger: Logger}} EconomySystemOptions
  */
@@ -46,7 +99,8 @@ class EconomySystem {
         } catch {
             this.setUser("852882606629847050", new XilefUser({
                 money: -1,
-                rank: -1
+                rank: -1,
+                vgot: new UserBinaryData("10313")
             }))
         }
     }
@@ -57,8 +111,13 @@ class EconomySystem {
         const deserialized = BSON.deserialize(rawFileContents)
 
         Object.keys(deserialized).forEach(key => {
-            this.#users.set(key, deserialized[key])
-            this.logger.fileSystemOperationSuccess(`Loaded <${key}> from economy.bson`)
+            const rawUserData = deserialized[key]
+
+            const userCreationOptions = {}
+            Object.assign(userCreationOptions, rawUserData)
+            
+            this.#users.set(key, new XilefUser(userCreationOptions))
+            this.logger.fileSystemOperationSuccess(`Loaded <${key}>from economy.bson \n(${JSON.stringify(userCreationOptions)})`)
         })
 
         this.logger.fileSystemOperationSuccess("Successfully loaded entire economy from economy.bson")
@@ -87,7 +146,6 @@ class EconomySystem {
 }
 
 module.exports = {
-    Balance,
     EconomySystem,
     XilefUser
 }
