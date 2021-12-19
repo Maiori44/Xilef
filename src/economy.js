9a05ad469291c8501802795a868e70c50f5c2682
unholy-codeism <re.nemesic@outlook.com>
2021-12-19 20:19:46 +0100

    fix: now use Long instead of Number as it works a bit better feat: add totalFlags to UserBinaryData -> update root user declaration feat: add "getEmptyUser" method for convenience purposes patch: getFormattedBinary now works properly

--- src/economy.js
+++ src/economy.js
@@ -4,16 +4,19 @@ const fs = require('fs')
 const BSON = require('bson')
 const saveFilePath = './src/data/economy.bson'
 
+const Long = BSON.Long
+
 class UserBinaryData {
     /**
      * 
      * @param {String} binary The binary read from the JSON
      */
-    constructor(binary) {
+    constructor(totalFlags, binary) {
         /**
-         * @type {Number}
+         * @type {Long}
          */
-        this.binary = binary ? Number(binary) : Number(0)
+        this.binary = binary ? Long(binary) : Long(0)
+        this.totalFlags = totalFlags
     }
 
     getBinary() {
@@ -22,18 +25,18 @@ class UserBinaryData {
 
     /**
      * Will set the bit at `position` (set to 1)
-     * @param {Number} position 
+     * @param {Long} position 
      */
-    setflagAt(position) { 
-        this.binary |= 1 << position // cool operator i found
+    setflagAt(position) {
+        this.binary |= Long(1) << Long(position) // cool operator i found
     }
 
     /**
      * Will clear the bit at `position` (set to 0)
-     * @param {Number} position 
+     * @param {Long} position 
      */
     clearFlagAt(position) {
-        this.binary &= ~(1 << position)
+        this.binary &= ~(Long(1) << Long(position))
     }
 
     /**
@@ -45,19 +48,29 @@ class UserBinaryData {
         return ((this.binary >> position) & 1) == 1
     }
 
+    /**
+     * 
+     * @param {Array<String>} replace1 
+     * @param {String} replace0 
+     * @returns {String}
+     */
     getFormattedBinary(replace1, replace0) {
         replace1 = replace1 || []
         replace1.reverse()
-        let bits = [...this.binary.toString(2).padStart(this.totalflags, "0")]
-        bits.forEach((bit, position) => {
-            if (bit == "1") {
-                bits[position] = replace1[position] || "1"
-            } else {
-                bits[position] = replace0 || "0"
-            }
-        })
+
+        const binaryString = this.binary.toString(2)
+        let bits = ("0".repeat(this.totalFlags - binaryString.length) + binaryString).split('')
+
+        for (let i = 0; i < bits.length; i++) {
+            if (bits[i] == "0")
+                bits[i] = replace0
+            else
+                bits[i] = replace1[i]
+        }
+
         replace1.reverse()
-        return bits.join("")
+
+        return bits.join('')
     }
 }
 
@@ -73,7 +86,18 @@ class XilefUser {
         this.money = options.money
         this.rank = options.rank
 
-        this.vgot = new UserBinaryData(options.vgot.binary || "0")
+        this.vgot = new UserBinaryData(60, options.vgot.binary ?? "0")
+
+        this.achievements = new UserBinaryData(9, options.achievements.binary ?? "0")
+    }
+
+    static getEmptyUser() {
+        return new XilefUser({
+            money: 0,
+            rank: 0,
+            vgot: new UserBinaryData(60, "0"),
+            achievements: new UserBinaryData(9, "0")
+        })
     }
 }
 
@@ -97,10 +121,12 @@ class EconomySystem {
         try {
             this.#loadBson()
         } catch {
-            this.setUser("852882606629847050", new XilefUser({
+            this.logger.warning("Creating XDT Framework user due to no data fetched from " + saveFilePath)
+            this.setUser("830008177156292609", new XilefUser({
                 money: -1,
                 rank: -1,
-                vgot: new UserBinaryData("10313")
+                vgot: new UserBinaryData(60, "1111"),
+                achievements: new UserBinaryData(9, "1111111")
             }))
         }
     }
@@ -115,9 +141,9 @@ class EconomySystem {
 
             const userCreationOptions = {}
             Object.assign(userCreationOptions, rawUserData)
-            
+
             this.#users.set(key, new XilefUser(userCreationOptions))
-            this.logger.fileSystemOperationSuccess(`Loaded <${key}>from economy.bson \n(${JSON.stringify(userCreationOptions)})`)
+            this.logger.fileSystemOperationSuccess(`Loaded <${key}> from economy.bson \n(${JSON.stringify(userCreationOptions)})`)
         })
 
         this.logger.fileSystemOperationSuccess("Successfully loaded entire economy from economy.bson")
@@ -129,13 +155,20 @@ class EconomySystem {
         this.logger.fileSystemOperationSuccess("Successfully saved entire economy to economy.bson")
     }
 
+    /**
+     * 
+     * @param {String} discordId 
+     * @returns Gets a user or creates it if it does not exist
+     */
     getUser(discordId) {
+        if (!this.#users.get(discordId) && typeof discordId == "string") {
+            this.setUser(discordId, XilefUser.getEmptyUser())
+        }
         return this.#users.get(discordId)
     }
 
     setUser(discordId, xilefUser) {
-        const user = this.#users.get(discordId)
-        if (!user) {
+        if (!this.#users.get(discordId)) {
             this.logger.instanceCreationSuccess(`Created a new instance of a user: \n<${discordId}> {\n ${JSON.stringify({ xilefUser })} \n}\n`)
         }
 
