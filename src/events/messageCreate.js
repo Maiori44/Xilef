8f19b97f845eda072ffe021cd7a387dae3ec49d1
unholy-codeism <87565960+unholy-codeism@users.noreply.github.com>
2021-12-20 23:08:23 +0100

    feat: a user may only run one command at a time; feat: command handling is now asynchronous

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,24 +1,33 @@
+const { Collection } = require('discord.js');
 const { Event } = require('../event.js')
 
-module.exports = new Event("messageCreate", (client, message) => {
+const runners = new Collection()
+
+module.exports = new Event("messageCreate", async (client, message) => {
     if (message.author.bot) return;
 
     if (!message.content.startsWith(client.prefix)) return;
 
     const args = message.content.substring(client.prefix.length).split(/ +/);
 
-    const command = client.commands.find(cmd => cmd.name == args[0]);
+    const command = await client.commands.find(cmd => cmd.name == args[0]);
 
     if (!command) return message.reply(`${args[0]} is not a valid command!`);
 
-    const permission = message.member.permissions.has(command.permission, true);
+    const permission = await message.member.permissions.has(command.permission, true);
 
     if (!permission)
         return message.reply(
             `You do not have the permission \`${command.permission}\` to run this command!`
         );
-    
+
     args.shift()
 
-    command.run(message, args, client);
+    if (runners.get(message.author.id) === true) return
+
+    runners.set(message.author.id, true)
+    command.run(message, args, client)
+        .then(() =>
+            runners.set(message.author.id, false)
+        )
 });
