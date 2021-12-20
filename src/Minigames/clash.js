04a00dc04f333c4ded533ff7d228104a9b4721c8
Felix44 <87673997+Felix-44@users.noreply.github.com>
2021-12-20 20:27:48 +0100

    fix bug with &clash build

--- src/Minigames/clash.js
+++ src/Minigames/clash.js
@@ -210,7 +210,7 @@ Commands.clash = new Command("Build your village and attack other's!\n\n" + Clas
 			const ClashMatrix = EconomySystem.clash
 			const building = Clash.buildings[args[1].toLowerCase()]
 			if (!building) {
-				message.channel.send("I have no clue what a " + args[1] + " is.\nIf you have no clue what you can build either, try `&clash buildings`".replace(/\&/g, Prefix.get(message.guild.id)))
+				return void message.channel.send("I have no clue what a " + args[1] + " is.\nIf you have no clue what you can build either, try `&clash buildings`".replace(/\&/g, Prefix.get(message.guild.id)))
 			}
 			const total = ClashMatrix.getTotal(building)
 			if (total == 30) {message.channel.send("You already build 30 of these, you can't build more"); return}
