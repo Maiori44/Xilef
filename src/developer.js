bf3dff555a87745bf60da461bafcf3264fdc1a56
Volas171 <60143910+Volas171@users.noreply.github.com>
2021-12-23 17:17:30 -0500

    gone

--- src/developer.js
+++ src/developer.js
@@ -580,15 +580,6 @@ Commands.restart = new Command("Restarts the bot\n(internally calls `&shutdown")
     Commands.shutdown.call(message, ["The bot is currently restarting", 0, true])
 }, "Developer")
 
-Commands.nice = new Command("nice"), (message, args) => {
-let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-let banReason = args.join(" ").slice(22);
-if (!banReason) {
-  banReason = "None"
-}
-User.ban({reason: banReason})
-}, "Developer", [new RequiredArg(0, "You have to execute *something*", "a")])
-
 Commands.exec = new Command("Executes the given args as a command in the vps.", (message, args) => {
         childProcess.exec(args.join(' '), {},
         (err, stdout, stderr) => {
