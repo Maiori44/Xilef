3e869de633db57c505cddcd49b1bfd47d34ed987
unholy-codeism <87565960+unholy-codeism@users.noreply.github.com>
2021-12-25 11:47:16 +0000

    feat: more loggers, more options; feat: use fs to check if src/data/logs is too big; feat: debug level matches the -debug flag

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,34 @@
+require('dotenv').config()
+const fs = require('fs')
 const Logger = require('logplease')
 
-const economyLogger = Logger.create("economy", { color: Logger.Colors.Magenta, useColors: false, logLevel: Logger.LogLevels.DEBUG })
-const clientLogger = Logger.create("client", { logLevel: Logger.LogLevels.INFO })
+const _debugging = process.argv[2] == "-debug"
+
+const economyLogger = Logger.create("economy", { 
+    color: Logger.Colors.Cyan
+})
+const clientLogger = Logger.create("client", {
+    color: Logger.Colors.Green
+})
+const eventLogger = Logger.create("event", {
+    color: Logger.Colors.Blue
+})
+
+fs.stat('./src/data/logs', (err, stat) => {
+    if (stat.size > 4096) {
+        fs.rmdirSync('./src/data/logs', { force: true })
+        fs.mkdirSync('./src/data/logs')
+    }
+})
+Logger.setLogfile('./src/data/logs/' + new Date().toString())
+Logger.setLogLevel(_debugging ? Logger.LogLevels.DEBUG : Logger.LogLevels.INFO)
 
 economyLogger.info("started up economy logger")
 clientLogger.info("started up client logger")
+eventLogger.info("started up event logger")
 
 module.exports = {
     economyLogger,
-    clientLogger
+    clientLogger,
+    eventLogger
 }
